167927a3b3d54ff5f9b21b47caf85668305c234c
kimminwoo123 <kmw0647@gmail.com>
2020-12-12 18:27:43 +0900

    2020/12/12

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,11 @@
+const { default: AppLayout } = require("../componets/AppLayout")
+
+const Home = () => {
+    return (
+       <AppLayout>
+            <div>Hello, Next</div> 
+       </AppLayout> 
+    )
+}
+
+export default Home
